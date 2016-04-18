3e9249d3a4204cd3953ecb16de2e472d4a3f96c0
Zach Barton <email": null>
2016-04-18 16:34:34 -0500

    Start student profile route

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  get "/students" => "students#index"
+  resources :students, only: [:index, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
