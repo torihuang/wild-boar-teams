e49c5591fbced473e5333cd7387e5b1bf88ac8fd
Byron Gage & Tori Huang <pair+byronbenjamin.torihuang@devbootcamp.com>
2016-04-18 16:33:42 -0500

    Complete student list functionality

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -2,17 +2,16 @@ $(document).ready(function() {
   $('#student-master-list').on('click', '.add-student-button', function(e) {
     e.preventDefault();
     var $this = $(this);
-    // var url = $this.attr();
     var splitURL = $this.attr('href').split('/');
-    var teamId = splitURL[2];
-    var studentId = splitURL[4];
+    var teamId = splitURL[1];
+    var studentId = splitURL[3];
     console.log(splitURL)
     $.ajax({
       method: "PUT",
       url: "/teams/" + teamId + "/students/" + studentId
     })
     .done(function(response) {
-
+      $this.remove();
     })
   })
 })
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,15 +2,12 @@ class TeamsController < ApplicationController
   def update
     team_id = params[:team_id]
     student_id = params[:student_id]
-    puts "teamid: #{params[:team_id]}"
-    puts "studentid: #{params[:student_id]}"
     team = Team.find_by(id: team_id)
     student = Student.find_by(id: student_id)
     if team && student
-      puts "student.team_id: #{student.team}"
       student.update(team_id: team_id)
       if request.xhr?
-        render 'students/summary'
+        render 'students/_summary', locals: {student: student}, layout: false
       else
         redirect_to "/students"
       end
@@ -18,10 +15,4 @@ class TeamsController < ApplicationController
       render '404'
     end
   end
-
-  # private
-
-  # def team_params
-  #   params.require(:team).permit(:team_id, :student_id)
-  # end
 end
