78fd697165d26e5af1b622e2ee0e6b1ffd401722
Heather Conklin <heath3conk@gmail.com>
2016-04-18 14:26:59 -0500

    Create models, associations, update schema

--- db/migrate/20160418182752_create_teams.rb
+++ db/migrate/20160418182752_create_teams.rb
@@ -2,7 +2,6 @@ class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
       t.integer  :teacher_id
-      t.integer  :student_id
       t.string  :name
 
       t.timestamps(null: false)
