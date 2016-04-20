b6f32af09dcf7c982ef8dc71eadd592bafddfdb5
torihuang <victoriahuang20@gmail.com>
2016-04-20 07:18:46 -0500

    Fix double alert bug

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $('.student-master-list').on('click', '.add-student-button', function(e) {
+  $('.add-student-button').on('click', function(e) {
     e.preventDefault();
     console.log("HERE")
     var $this = $(this);
@@ -11,12 +11,15 @@ $(document).ready(function() {
     $.ajax({
       type: "PUT",
       beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-      url: "/students/2/teams/10"
+      url: $this.attr('href')
     })
     .done(function(response) {
       console.log(response);
       var name = $this.parent().parent().parent().find('#student-name').text();
-      $this.parent().parent().parent().replaceWith(response);
+      $parent = $this.parent().parent().parent()
+      console.log($parent)
+      $parent.replaceWith(response);
+      // if ($parent.find('.add-student-button'))
       alert(name + " was added to your team!");
     })
   });
