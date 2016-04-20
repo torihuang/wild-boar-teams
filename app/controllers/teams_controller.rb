dd3b18f843c22ef753ca0c0aa552f9920be8e715
Heather Conklin <heath3conk@gmail.com>
2016-04-20 13:28:48 -0500

    Update teams controller & add partial for eligible students

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,21 +1,20 @@
 class TeamsController < ApplicationController
   def update
-    # p "hello world"
     team_id = params[:id]
     student_id = params[:student_id]
-    puts "team_id: #{team_id}"
-    puts "student_id: #{student_id}"
     team = Team.find_by(id: team_id)
     student = Student.find_by(id: student_id)
-    puts student
-    puts team
     if team && student
-      student.update(team_id: team_id)
-      if request.xhr?
-        render 'students/_summary', locals: {student: student}, layout: false
-        # student.team.name
+      if team.allow_update(student)[0] == true
+        student.update(team_id: team_id)
+        if request.xhr?
+          render 'students/_summary', locals: {student: student}, layout: false
+          # student.team.name
+        else
+          redirect_to "/students"
+        end
       else
-        redirect_to "/students"
+        render '422'
       end
     else
       render '404'
